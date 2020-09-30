6ef6a7c74aed0ab6348d31aab66e6b4db11b91c0
Alexander Rogalsky <arogalsky@innotechnum.com>
2020-09-30 13:10:27 +0300

    Updates on entrypoint.sh

--- modules/document-generator/src/main/docker/java/entrypoint.sh
+++ modules/document-generator/src/main/docker/java/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-while ! nc -z config-server 8888 ; do
-    echo "Waiting for upcoming Config Server"
-    sleep 2
-done
-
 # Java environment options
 ENV JAVA_VM_OPTS="-Xms64m -Xmx1024m -XX:MetaspaceSize=96M -XX:MaxMetaspaceSize=256m -XX:NativeMemoryTracking=summary -XX:+UseConcMarkSweepGC"
 ENV JAVA_OPTS="-Dspring.profiles.active=default -Djava.security.egd=file:/dev/./urandom -Djava.net.preferIPv4Stack=true -Dcom.sun.xml.bind.v2.bytecode.ClassTailor.noOptimize=true"
